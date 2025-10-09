fa0157c0dc5a44a7d9fa4f7ada2305e8ba226632
ticinovicsara <ticinovicsara0@gmail.com>
2025-10-09 18:44:57 +0200

    small fix

--- apps/backend/src/routes/userRouts.js
+++ apps/backend/src/routes/userRouts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers, createUser } from "./controllers/userController.js";
+import { getUsers, createUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
