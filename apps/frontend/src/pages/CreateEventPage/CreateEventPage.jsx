00202b4f1404f09016bda378bcfffd862aaafa66
Natali933 <natalipesic2005@gmail.com>
2025-11-28 12:07:40 +0100

    pocetni botun, samo pouksavan nesto

--- apps/frontend/src/pages/CreateEventPage/CreateEventPage.jsx
+++ apps/frontend/src/pages/CreateEventPage/CreateEventPage.jsx
@@ -1,9 +1,22 @@
 import "./createEventPage.css";
+import { CreateButton } from '../../components/CreateButton';
 
 function CreateEventPage() {
   return (
     <div className="create-event-page">
       <h1>CreateEventPage</h1>
+      <h2>novi naslov</h2>
+
+     {/* Sticky gumb */}
+      <div style={{
+        position: 'fixed',
+        bottom: '20px',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+      }}>
+        <CreateButton text="Klikni me" />
+      </div>
     </div>
   );
 }
