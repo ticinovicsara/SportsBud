7d1f6ded86c2d21206cf618545d692cd477d20f6
ticinovicsara <ticinovicsara0@gmail.com>
2025-12-09 22:58:39 +0100

    add color and links to icons

--- apps/frontend/src/components/NavBar/NavBar.jsx
+++ apps/frontend/src/components/NavBar/NavBar.jsx
@@ -1,22 +1,38 @@
 import styles from './navbar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faCirclePlus, faUser } from '@fortawesome/free-solid-svg-icons';
+import { useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
   return (
     <nav className={styles['navbar-container']}>
       <ul className={styles['navbar-items']}>
-        <li className={styles['navbar-item']}>
-          <FontAwesomeIcon icon={faHouse} className={styles['navbar-icon']} />
-          <p>Home</p>
+        <li className={`${styles['navbar-item']} ${isActive('/') ? styles.active : ''}`}>
+          <NavLink to="/" end>
+            <FontAwesomeIcon icon={faHouse} className={styles['navbar-icon']} />
+            <p>Home</p>
+          </NavLink>
         </li>
-        <li className={styles['navbar-item']}>
-          <FontAwesomeIcon icon={faCirclePlus} className={styles['navbar-icon']} />
-          <p>Create</p>
+        <li
+          className={`${styles['navbar-item']} ${isActive('/create-event') ? styles.active : ''}`}
+        >
+          <NavLink to="/create-event" end>
+            <FontAwesomeIcon icon={faCirclePlus} className={styles['navbar-icon']} />
+            <p>Create</p>
+          </NavLink>
         </li>
-        <li className={styles['navbar-item']}>
-          <FontAwesomeIcon icon={faUser} className={styles['navbar-icon']} />
-          <p>Profile</p>
+        <li className={`${styles['navbar-item']} ${isActive('/profile') ? styles.active : ''}`}>
+          <NavLink to="/profile" end>
+            <FontAwesomeIcon icon={faUser} className={styles['navbar-icon']} />
+            <p>Profile</p>
+          </NavLink>
         </li>
       </ul>
     </nav>
